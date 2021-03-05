76cf663be0da109a592c6d95366ead217b06a9a9
Amanda Rolon <abrolon87@gmail.com>
2021-03-04 19:55:44 -0500

    edits paths for images

--- alien.py
+++ alien.py
@@ -1,18 +1,19 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Alien(Sprite):
-    #class to represent single alien in fleet
+    # class to represent single alien in fleet
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
 
-        #load alien image and set rect attribute
-        self.image = pygame.image.load('images/alien.bmp')
+        # load alien image and set rect attribute
+        self.image = pygame.image.load('alien.bmp')
         self.rect = self.image.get_rect()
 
-        #start new alien at top left of screen
+        # start new alien at top left of screen
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
@@ -20,16 +21,12 @@ class Alien(Sprite):
         self.x = float(self.rect.x)
 
     def check_edges(self):
-        #return TRUE if alien is at edge of screen
+        # return TRUE if alien is at edge of screen
         screen_rect = self.screen.get_rect()
         if self.rect.right >= screen_rect.right or self.rect.left <= 0:
             return True
 
     def update(self):
-        #move alien to right or left
+        # move alien to right or left
         self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
-
-    
-        
-
--- ship.py
+++ ship.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Ship:
 
     def __init__(self, ai_game):
@@ -7,17 +8,17 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
-        
+
         # load ship
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('ship.bmp')
         self.rect = self.image.get_rect()
         # start new ships at midbottom of screen
         self.rect.midbottom = self.screen_rect.midbottom
 
         # store decimal value for horizontal position
         self.x = float(self.rect.x)
-        
-        #movement flag
+
+        # movement flag
         self.moving_right = False
         self.moving_left = False
 
@@ -27,9 +28,8 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-            
 
-        self.rect.x = self.x #something is up with this line
+        self.rect.x = self.x  # something is up with this line
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
