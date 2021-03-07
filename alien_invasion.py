bc0939641bb613b4609b536d462ef0d93beb9df6
Amanda Rolon <abrolon87@gmail.com>
2021-03-06 19:31:57 -0500

    sets cursor to be visible when game ends

--- alien_invasion.py
+++ alien_invasion.py
@@ -202,6 +202,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         screen_rect = self.screen.get_rect()
